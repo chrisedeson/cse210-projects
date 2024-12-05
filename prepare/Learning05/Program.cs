f319097a67f829a9848afafa6911fd565fb6d9b9
Christopher <edesonchristopher@gmail.com>
2024-12-05 11:36:15 +0100

    finished learning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+
+        Assignment first = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(first.GetSummary());
+
+        Console.WriteLine();
+
+        MathAssignment second = new MathAssignment("7.3", "8-19", "Roberto Carlos", "Fractions");
+        // Console.WriteLine(second.GetSummary());
+        Console.WriteLine(second.GetHomeworkList());
+
+        Console.WriteLine();
+        WritingAssignment third = new WritingAssignment("The Causes of World War II", "Mary Waters", "European History");
+        Console.WriteLine(third.GetWritingInformation());
     }
 }
